a42d41220f91d60686cb6c91efc9b9935693d0a2
Arian Rahbar <arahbar@usc.edu>
2020-12-27 02:05:18 -0600

    first attempt

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+var upload = require('multer')();
+require('dotenv').config();
+
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.use(upload.single('upfile'));
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyze', function (req, res) {
+  const { originalname, mimetype, size } = req.file
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+})
+
+
+
 
 
 
